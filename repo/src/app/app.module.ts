29d6f185b7182284c3742c64be843bc638a8fc2e
Amrit Kharbanda <amrkharb@publicisgroupe.net>
2018-05-21 09:28:05 +0530

    register component

--- repo/src/app/app.module.ts
+++ repo/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { UserDisplayComponent } from './components/user-display/user-display.component';
 import { HelpdeskDisplayComponent } from './components/helpdesk-display/helpdesk-display.component';
+import { AppRoutingModule } from './Routes/Routes';
+import { ApiserviceService } from './Services/apiservice.service';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
@@ -16,14 +19,16 @@ import { HelpdeskDisplayComponent } from './components/helpdesk-display/helpdesk
     LoginComponent,
     RegisterComponent,
     UserDisplayComponent,
-    HelpdeskDisplayComponent
+    HelpdeskDisplayComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ApiserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- repo/src/app/components/register/register.component.ts
+++ repo/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { ApiserviceService } from '../../Services/apiservice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  register : FormGroup;
+  country: any;
+
+  constructor(
+    private api : ApiserviceService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.registerForm();
+    this.getCountry();
   }
 
+  registerForm = () => {
+    this.register = new FormGroup({
+      fName: new FormControl('', [Validators.required]),
+      lname: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required]),
+      countryId: new FormControl('', [Validators.required]),
+      userName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      role: new FormControl('', [Validators.required])
+    });
+  }
+ 
+  getCountry = () => {
+    this.api.getCountry().subscribe(res => this.country = res);
+  }
+
+  registerSubmit = () => {
+    console.log(this.register.value);
+    this.router.navigateByUrl('');
+  }
 }
